116cc042f574608b316931ab4e5710f227f29746
Katastropha <weissjulia1991@gmail.com>
2024-02-15 15:22:35 +0100

    add icon, add function that return icons and descriptions, fix states in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+// import { weatherCode } from "./weatherCode";
+import { getWeatherIcon } from "./getWeatherIcon";
+
+interface weatherDataI {
+  time: string;
+  interval: number;
+  temperature_2m: number;
+  apparent_temperature: number;
+  is_day: boolean;
+  weather_code: number | string;
+}
 
 const api = "https://api.open-meteo.com/v1/dwd-icon?";
 
@@ -9,55 +20,19 @@ const api = "https://api.open-meteo.com/v1/dwd-icon?";
 // current=temperature_2m,                        - CURRENT TEMPERATURE
 // apparent_temperature,                          - APPARENT TEMPERATURE
 // is_day&                                        - TIME OF DAY
-// hourly=temperature_2m&                         - HOURLY
 // forecast_days=1                                - DAYS
 
 // https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_gusts_10m&forecast_days=1
 
-const weatheCode = {
-  0: "Clear sky",
-  1: "Mainly clear",
-  2: "Partly cloudy",
-  3: "Overcast sky",
-  45: "Fog",
-  48: "Depositing rime fog",
-  51: "Light drizzle",
-  53: "Moderate drizzle",
-  55: "Dense intensity drizzle",
-  56: "Light freezing drizzle",
-  57: "Dense intensity freezing drizzle",
-  61: "Slight rain",
-  63: "Moderate rain",
-  65: "Heavy intensity rain",
-  66: "Light freezing rain",
-  67: "Heavy intensity freezing rain",
-  71: "Slight snow fall",
-  73: "Moderate snow fall",
-  75: "Heavy intensity snow fall ",
-  77: "Snow grains",
-  80: "Slight rain showers",
-  81: "Moderate rain showers",
-  82: "Violent rain showers",
-  85: "Slight snow showers slight",
-  86: "Heavy snow showers heavy",
-  95: "Slight thunderstorm",
-  "95 *": "Moderate thunderstorm",
-  96: "Thunderstorm with slight hail",
-  "99 *": "Thunderstorm with heavy hail",
-};
-
 function App() {
-  const [temperature, setTemperature] = useState("");
-  const [apparentTemperature, setApparentTemperature] = useState("");
   const [position, setPosition] = useState([]);
-  const [time, setTime] = useState("forecast_days=1");
   const [isPushed, setIsPushed] = useState(false);
-  const [icon, setIcon] = useState("");
+  const [weather, setWeather] = useState<weatherDataI>();
+  const [icon, setIcon] = useState({});
 
   const handlePosition = () => {
     navigator.geolocation.getCurrentPosition((pos) => {
       const result: number[] = [pos.coords.latitude, pos.coords.longitude];
-
       setPosition(result);
       setIsPushed(true);
     });
@@ -69,8 +44,9 @@ function App() {
     )
       .then((res) => res.json())
       .then((data) => {
-        setTemperature(data.current.temperature_2m);
-        setApparentTemperature(data.current.apparent_temperature);
+        setWeather(data.current);
+        setIcon(getWeatherIcon(data.current.weather_code, data.current.is_day));
+        console.log(icon);
       });
   }, [position]);
 
@@ -78,11 +54,13 @@ function App() {
     <div>
       {isPushed ? (
         <div className="weather">
+          <div className={`icon ${icon.icon}`}></div>
           <h1 className="weather__temperature">
-            The temperature in your region is {temperature} °C
+            The temperature in your region is {weather?.temperature_2m} °C
           </h1>
           <p className="weather__apparentTemperature">
-            The apparent temperature is {apparentTemperature}
+            {icon.massage}. The apparent temperature is{" "}
+            {weather?.apparent_temperature} °C
           </p>
         </div>
       ) : (
---
+++ src/getWeatherIcon.ts
@@ -0,0 +1,73 @@
+import { weatherCode } from "./weatherCode";
+
+export interface waetherIconI {
+  icon: string;
+  massage: string;
+}
+
+const weatherInfo = {
+  0: {
+    class: "day-clear",
+    massage: "Clear sky",
+  },
+
+  1: { class: "partly-cloudy", massage: "Mainly clear" },
+  2: { class: "partly-cloudy", massage: "Partly cloudy" },
+  3: { class: "clouds", massage: "Overcast sky" },
+
+  45: { class: "fog", massage: "Fog" },
+  48: { class: "fog", massage: "Depositing rime fog" },
+
+  51: { class: "day rain", massage: "Light drizzle" },
+  53: { class: "day rain", massage: "Moderate drizzle" },
+  55: { class: "day rain rainwater", massage: "Dense intensity drizzle" },
+  56: { class: "day rain snow", massage: "Light freezing drizzle" },
+  57: { class: "day rain snow", massage: "Dense intensity freezing drizzle" },
+
+  61: { class: "rain rainbow", massage: "Slight rain" },
+  63: { class: "rain", massage: "Moderate rain" },
+  65: { class: "rainwater", massage: "Heavy intensity rai" },
+  66: { class: "rain snow", massage: "Light freezing rain" },
+  67: { class: "rainwater snow", massage: "Heavy intensity freezing rain" },
+
+  71: { class: "day snow", massage: "Slight snow fall" },
+  73: { class: "snow", massage: "Moderate snow fall" },
+  75: { class: "snow rainwater", massage: "Heavy intensity snow fall" },
+
+  77: { class: "snow snow-storm", massage: "Snow grains" },
+  80: { class: "rainwater", massage: "Slight rain showers" },
+  81: { class: "rainwater ", massage: "Moderate rain showers" },
+  82: { class: "rainwater storm", massage: "Violent rain showers" },
+  85: { class: "snow storm", massage: "Slight snow showers slight" },
+  86: { class: "snow snow-storm", massage: "Heavy snow showers heavy" },
+  95: { class: "snow-storm", massage: "Slight thunderstorm" },
+  "95 *": { class: "snow-storm storm ", massage: "Moderate thunderstorm" },
+  96: { class: "snow-storm storm", massage: "Thunderstorm with slight hail" },
+  "99 *": {
+    class: "snow-storm rainwater storm",
+    massage: "Hunderstorm with heavy hail",
+  },
+};
+
+export const getWeatherIcon = (
+  code: string | number,
+  isDay: boolean
+): waetherIconI => {
+  let className = "";
+
+  if (isDay) className += "day ";
+  else className += "night ";
+
+  if (code === 0 && isDay) {
+    className += "day-clear";
+  } else if (code === 0 && !isDay) {
+    className += "night-clear";
+  } else {
+    className += weatherInfo[code].class;
+  }
+
+  return {
+    icon: className,
+    massage: weatherInfo[code].massage,
+  };
+};
